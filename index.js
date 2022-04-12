092524faab793635c07699ada3c3a146cd559e4f
Sergio <kurisergio777777@gmail.com>
2022-04-12 13:34:11 -0400

    commit tree d1dbcc5e8b4af9588e9f5ec189f4512141355310

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+ let button = document.querySelector("#input")
+ button.addEventListener("click", ()=>{
+     alert(`Ìwas clicked`)
+
+ })}
+
+
+
+ addingEventListener()
